fd102978568629c04930d92f105fa472927394d4
Ben Aguirre <ben.aguirre03@gmail.com>
2023-03-20 18:35:48 -0500

    Add project constructor

---
+++ src/project.js
@@ -0,0 +1,19 @@
+class Project {
+  constructor(
+    title,
+    description,
+    dueDate,
+    priority,
+    timeAllocate,
+    notes,
+    checkStat
+  ) {
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+    this.timeAllocate = timeAllocate;
+    this.notes = notes;
+    this.checkStat = checkStat;
+  }
+}
