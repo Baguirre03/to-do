32ea1aaad11fd966a6bbcc742342e9d771bc22a7
Ben Aguirre <ben.aguirre03@gmail.com>
2023-03-22 14:03:27 -0500

    Clean out code

--- src/index.js
+++ src/index.js
@@ -1,27 +1,4 @@
 import pageLoad from "./pageLoad";
-import { allProjects, deleteProject } from "./project";
-import projectSort from "./projectAssign";
 import "./style.css";
-import { allToDo, createNewToDo, deleteTask } from "./toDo";
 
 pageLoad();
-createNewToDo(
-  "1proj2Ass(1)",
-  "get this  done by friday",
-  "11/11",
-  "Now",
-  "allocate 2 hours",
-  "note here",
-  "done",
-  "proj2"
-);
-createNewToDo(
-  "2proj2Ass(1)",
-  "get this  done by friday",
-  "11/11",
-  "Now",
-  "allocate 2 hours",
-  "note here",
-  "done",
-  "projectt"
-);
