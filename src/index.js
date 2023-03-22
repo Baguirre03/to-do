fc6bae7fca214d529921a8a087017246d2f86317
Ben Aguirre <ben.aguirre03@gmail.com>
2023-03-22 13:21:12 -0500

    Clean up code to test

--- src/index.js
+++ src/index.js
@@ -14,44 +14,4 @@ createNewToDo(
   "done",
   "proj2"
 );
-createNewToDo(
-  "2proj2 assign",
-  "get this  done by friday",
-  "11/11",
-  "Now",
-  "allocate 2 hours",
-  "note here",
-  "done",
-  "proj2"
-);
-createNewToDo(
-  "3projecttAssign",
-  "get this  done by friday",
-  "11/11",
-  "Now",
-  "allocate 2 hours",
-  "note here",
-  "done",
-  "projectt"
-);
-createNewToDo(
-  "4projectttAss(2)",
-  "get this  done by friday",
-  "11/11",
-  "Now",
-  "allocate 2 hours",
-  "note here",
-  "done",
-  "projectt"
-);
-createNewToDo(
-  "5proj2",
-  "get this  done by friday",
-  "11/11",
-  "Now",
-  "allocate 2 hours",
-  "note here",
-  "done",
-  "proj2"
-);
 assignToDo();
--- src/projectAssign.js
+++ src/projectAssign.js
@@ -4,21 +4,18 @@ import { allProjects, createProject } from "./project";
 
 // defaultToDo - Holds All
 createProject("default");
-createProject("proj2");
-createProject("projectt");
 const assignToDo = () => {
-  for (let i = 0; i < allToDo.length; i++) {
+  for (let i = 0; i < allProjects.length; i++) {
     const projects = allProjects[i];
+    if (projects === undefined) {
+      return;
+    }
     for (let j = 0; j < allToDo.length; j++) {
-      if (projects === undefined) {
-        return;
-      }
       if (allToDo[j].project === projects.name) {
         projects.projectToDo.push(allToDo[j]);
       }
     }
   }
 };
-console.log(allProjects);
 
 export default assignToDo;
