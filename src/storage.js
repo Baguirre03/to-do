fd7e3226b081fd9b1c4de5f003d572c64ee9c611
Ben Aguirre <ben.aguirre03@gmail.com>
2023-03-29 11:36:05 -0500

    clear logs

--- src/storage.js
+++ src/storage.js
@@ -7,12 +7,10 @@ import { allToDo, defaultTasks, ToDo } from "./toDo";
 // Task storage
 const addTasksToStorage = () => {
   localStorage.setItem("allTasks", JSON.stringify(allToDo));
-  console.log("allTasksStored :", localStorage.getItem("allTasks"));
 };
 
 const displayTasksFromStore = () => {
   const storageData = JSON.parse(localStorage.getItem("allTasks"));
-  console.log("storageData", storageData);
 
   storageData.map((data) =>
     allToDo.push(
@@ -35,26 +33,21 @@ const displayTasksFromStore = () => {
 const checkStorage = () => {
   const storage = localStorage.getItem("allTasks");
   if (storage === null) {
-    console.log("empty");
     defaultTasks();
     addTasksToStorage();
     sortAndDisplayTasks("0");
   } else if (storage.length !== 0) {
     displayTasksFromStore();
-  } else {
-    console.log("error");
   }
 };
 
 // Project storage
 const addProjectsToStore = () => {
   localStorage.setItem("projects", JSON.stringify(allProjects));
-  console.log("allProjects :", localStorage.getItem("projects"));
 };
 
 const displayProjectsFromStorage = () => {
   const storageData = JSON.parse(localStorage.getItem("projects"));
-  console.log("Projects Data:", storageData);
 
   storageData.map((data) =>
     allProjects.push(new Project(data.name, data.projectToDo))
@@ -66,14 +59,11 @@ const displayProjectsFromStorage = () => {
 const checkProjStorage = () => {
   const storage = localStorage.getItem("projects");
   if (storage === null) {
-    console.log("default projects display");
     defaultProjects();
     addProjectsToStore();
     clearAndDisplayProjects();
   } else if (storage.length !== 0) {
     displayProjectsFromStorage();
-  } else {
-    console.log("error");
   }
 };
 
