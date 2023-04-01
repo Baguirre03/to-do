c6cf1fa43b248d34d719f13939898367d780b077
Ben Aguirre <ben.aguirre03@gmail.com>
2023-04-01 14:58:04 -0500

    fix issue with button propogation

--- src/displayAllTasks.js
+++ src/displayAllTasks.js
@@ -29,7 +29,7 @@ const getEventListeners = () => {
   const deleteButtons = document.querySelectorAll(".delete-btn");
   deleteButtons.forEach((btn) => {
     btn.addEventListener("click", (event) => {
-      deleteTask(event.target.id);
+      deleteTask(event.currentTarget.id);
       addTasksToStorage();
       sortAndDisplayTasks(currentProject);
     });
@@ -38,7 +38,7 @@ const getEventListeners = () => {
   const getInfoButtons = document.querySelectorAll(".info-btn");
   getInfoButtons.forEach((btn) => {
     btn.addEventListener("click", (event) => {
-      const task = allToDo[event.target.id];
+      const task = allToDo[event.currentTarget.id];
       console.log(task.getInfo());
     });
   });
@@ -64,9 +64,9 @@ const disiplayOnDOM = (indexDisplay, i) => {
   <div class="right-task-items">
   <div class="priority"><p>Priority: ${indexDisplay.priority}</p></div>
   <div class="done"><p>Complete ${indexDisplay.checkStat}</p></div>
-  <div class="edit"><button class=edit-task id=${indexOfTask}><svg id="edit-image" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M18.13 12L19.39 10.74C19.83 10.3 20.39 10.06 21 10V9L15 3H5C3.89 3 3 3.89 3 5V19C3 20.1 3.89 21 5 21H11V19.13L11.13 19H5V5H12V12H18.13M14 4.5L19.5 10H14V4.5M19.13 13.83L21.17 15.87L15.04 22H13V19.96L19.13 13.83M22.85 14.19L21.87 15.17L19.83 13.13L20.81 12.15C21 11.95 21.33 11.95 21.53 12.15L22.85 13.47C23.05 13.67 23.05 14 22.85 14.19Z" /></svg></button></div>
-  <div class="delete"><button class=delete-btn id=${indexOfTask}><svg id="delete-img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M9,7H11L12,9.5L13,7H15L13,12L15,17H13L12,14.5L11,17H9L11,12L9,7M12,2A10,10 0 0,1 22,12A10,10 0 0,1 12,22A10,10 0 0,1 2,12A10,10 0 0,1 12,2M12,4A8,8 0 0,0 4,12A8,8 0 0,0 12,20A8,8 0 0,0 20,12A8,8 0 0,0 12,4Z" /></svg></button></div>
-  <div class="get-information"><button class=info-btn id=${indexOfTask}><svg id="info-image" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M5 3H19C20.1 3 21 3.89 21 5V19C21 19.53 20.79 20.04 20.41 20.41C20.04 20.79 19.53 21 19 21H5C4.47 21 3.96 20.79 3.59 20.41C3.21 20.04 3 19.53 3 19V5C3 3.89 3.89 3 5 3M13 9V7H11V9H13M13 17V11H11V17H13Z" /></svg></button></div>
+  <div class="edit"><button class=edit-task id=${indexOfTask}><svg class="edit-image" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M18.13 12L19.39 10.74C19.83 10.3 20.39 10.06 21 10V9L15 3H5C3.89 3 3 3.89 3 5V19C3 20.1 3.89 21 5 21H11V19.13L11.13 19H5V5H12V12H18.13M14 4.5L19.5 10H14V4.5M19.13 13.83L21.17 15.87L15.04 22H13V19.96L19.13 13.83M22.85 14.19L21.87 15.17L19.83 13.13L20.81 12.15C21 11.95 21.33 11.95 21.53 12.15L22.85 13.47C23.05 13.67 23.05 14 22.85 14.19Z" /></svg></button></div>
+  <div class="get-information"><button class=info-btn id=${indexOfTask}><svg class="info-image" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M5 3H19C20.1 3 21 3.89 21 5V19C21 19.53 20.79 20.04 20.41 20.41C20.04 20.79 19.53 21 19 21H5C4.47 21 3.96 20.79 3.59 20.41C3.21 20.04 3 19.53 3 19V5C3 3.89 3.89 3 5 3M13 9V7H11V9H13M13 17V11H11V17H13Z" /></svg></button></div>
+  <div class="delete"><button class=delete-btn id=${indexOfTask}><svg class="delete-img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M9,7H11L12,9.5L13,7H15L13,12L15,17H13L12,14.5L11,17H9L11,12L9,7M12,2A10,10 0 0,1 22,12A10,10 0 0,1 12,22A10,10 0 0,1 2,12A10,10 0 0,1 12,2M12,4A8,8 0 0,0 4,12A8,8 0 0,0 12,20A8,8 0 0,0 20,12A8,8 0 0,0 12,4Z" /></svg></button></div>
   </div>`;
 };
 
