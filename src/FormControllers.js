ee36fa09a6db128b94724036a2c75cc878dbe006
Ben Aguirre <ben.aguirre03@gmail.com>
2023-04-16 14:10:25 -0500

    add dropdown for mobile

--- src/FormControllers.js
+++ src/FormControllers.js
@@ -258,6 +258,18 @@ const eventListeners = () => {
   logo.addEventListener("click", () => {
     document.querySelector("#all").click();
   });
+
+  const dropDown = document.querySelector(".dropdown");
+  dropDown.addEventListener("click", () => {
+    const sidebar = document.querySelector(".sidebar");
+    if (sidebar.classList.contains("active-dropdown")) {
+      sidebar.classList.remove("active-dropdown");
+      dropDown.innerHTML = `<svg id="dropdown-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>arrow-down-drop-circle-outline</title><path d="M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2M12,4A8,8 0 0,1 20,12A8,8 0 0,1 12,20A8,8 0 0,1 4,12A8,8 0 0,1 12,4M7,10L12,15L17,10H7Z" /></svg>`;
+    } else {
+      sidebar.classList.add("active-dropdown");
+      dropDown.innerHTML = `<svg id="dropdown-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>arrow-up-drop-circle-outline</title><path d="M12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22M12,20A8,8 0 0,1 4,12A8,8 0 0,1 12,4A8,8 0 0,1 20,12A8,8 0 0,1 12,20M17,14L12,9L7,14H17Z" /></svg>`;
+    }
+  });
 };
 
 export {
